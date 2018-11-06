68e93af9bbd77794ae8674485d135301b975e233
Mohamed <Ethical.Hacker720@gmail.com>
2018-11-06 08:41:13 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # configure the repository file
-echo "deb http://http.kali.org/kali kali-rolling main contrib non-free" > /etc/apt/sources.list
+echo "deb http://http.kali.org/kali kali-rolling main contrib non-free" >> /etc/apt/sources.list
 
 # install Tor && requirements
 apt-get update && apt-get install tor -y && apt autoremove -y
